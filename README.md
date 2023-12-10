c76061af5dd3d3dbe7d4078ee7028041a5ea70e2
Kingtouan1703 <redutdep13@gmail.com>
2023-12-10 09:06:11 +0700

    learn Result type

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rust-chatapp
 
 Url google stand :https://developers.google.com/search/docs/crawling-indexing/url-structure
+
+DB mongodb
+JSON respone standard :https://github.com/omniti-labs/jsend
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use axum::{
     extract::State,
     response::Html,
+    response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
 use dotenv::dotenv;
-use mongodb::{options::ClientOptions, Client, Database};
+use mongodb::{bson::doc, options::ClientOptions, Client, Database};
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
@@ -16,16 +18,45 @@ async fn main() {
         .route("/", get(root))
         .route("/auth/signin", post(signin))
         .with_state(db);
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct SigninPayload {
     username: String,
+    password: String,
 }
 
-async fn signin(db: State<Database>, Json(payload): Json<SigninPayload>) {
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    username: String,
+    password: String,
+}
+async fn signin(
+    State(db): State<Database>,
+    Json(payload): Json<SigninPayload>,
+) -> Result<Json<Value>, Json<Value>> {
     println!("{:?} ", payload);
+    let payload_username = payload.username;
+    let payload_password: String = payload.password;
+    let user_collection: mongodb::Collection<User> = db.collection("user");
+    let result = user_collection
+        .find_one(doc! { "username": payload_username }, None)
+        .await;
+
+    match result {
+        Ok(Some(_user)) => Err(Json(json!(
+        { "data": "username is existed","status": "fail",}
+        ))),
+        Ok(None) => Ok(Json(json!({ "data": "username is existed" }))),
+        Err(e) => {
+            println!("error parsing header: {e:?}");
+            Err(Json(
+                json!({ "data": "something went wrong","status": "fail" }),
+            ))
+        }
+    }
+
     // check had username
     // create a new user document ;
 }
@@ -35,7 +66,7 @@ async fn root() -> Html<&'static str> {
 
 async fn connect_mongodb() -> mongodb::Database {
     let mongodb_uri = std::env::var("MONGO_DB_URI").expect("MONGO_DB_URI must be set.");
-    let db_name = std::env::var("DB_NAME").expect("MONGO_DB_URI must be set.");
+    let db_name = std::env::var("DB_NAME").expect("DB NAME must be set.");
     let client_options = ClientOptions::parse(mongodb_uri)
         .await
         .expect("Can not parse client options");
